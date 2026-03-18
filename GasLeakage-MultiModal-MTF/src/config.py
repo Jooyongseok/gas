7f9dd6d6de4999e33a60e5dfd367d0f5f240b4df
Jooyongseok <jys0207@gmail.com>
2026-03-18 19:48:21 +0900

    config.py 데이터 경로 수정: /data2 → /project/ahnailab/data/gas

--- GasLeakage-MultiModal-MTF/src/config.py
+++ GasLeakage-MultiModal-MTF/src/config.py
@@ -1,14 +1,15 @@
 import torch
 
-DATA_DIR_SENSOR = '/data2/project/2025summer/jjh0709/Data/zkwgkjkjn9-2/Gas Sensor HM'
-DATA_DIR_SENSOR_CSV ='path = "/data2/project/2025summer/jjh0709/Data/zkwgkjkjn9-2/Gas Sensors Measurements/Gas_Sensors_Measurements.csv'
-DATA_DIR_THERMAL = '/data2/project/2025summer/jjh0709/Data/zkwgkjkjn9-2/Thermal Camera Images'
+DATA_DIR_SENSOR   = '/project/ahnailab/data/gas/Gas Sensor HM'
+DATA_DIR_THERMAL  = '/project/ahnailab/data/gas/Thermal Camera Images'
+DATA_DIR_SENSOR_CSV = '/project/ahnailab/data/gas/Gas_Sensors_Measurements.csv'
 
-BATCH_SIZE = 8
-EPOCHS = 3
+BATCH_SIZE  = 8
+EPOCHS      = 3
 NUM_CLASSES = 4
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 LABEL_MAP = {'Mixture': 0, 'NoGas': 1, 'Perfume': 2, 'Smoke': 3}
-ORIGINAL_DATA ="/data2/project/2025summer/jjh0709/Data/zkwgkjkjn9-2/Gas Sensors Measurements/Gas_Sensors_Measurements.csv"
-TRAIN_CSV_PATH="/data2/project/2025summer/jjh0709/git/GasLeakage-MultiModal-MTF/data/TRAIN_DATA.csv"
-TEST_CSV_PATH="/data2/project/2025summer/jjh0709/git/GasLeakage-MultiModal-MTF/data/TEST_DATA.csv"
+
+ORIGINAL_DATA  = '/project/ahnailab/data/gas/Gas_Sensors_Measurements.csv'
+TRAIN_CSV_PATH = '/project/ahnailab/data/gas/TRAIN_DATA.csv'
+TEST_CSV_PATH  = '/project/ahnailab/data/gas/TEST_DATA.csv'
